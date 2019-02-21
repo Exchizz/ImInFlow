0354492267fe8175cbd6f068c32fca78ad411388
Mathias Neerup <mane@mmmi.sdu.dk>
2019-02-21 14:33:59 +0100

    Meh

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ var HOST = '127.0.0.1';
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
-var color = null;
+var state = 'off';
+var myTimer = null;
 
 server.on('listening', function () {
     var address = server.address();
@@ -19,24 +20,45 @@ server.on('listening', function () {
 
 server.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
-    color = message;
+    var msgObj = null;
+
+    try {
+      msgObj = JSON.parse(message);
+    } catch (e) {
+      server.send("Error: Unable to decode json message",remote.port,HOST,function(error){});
+    }
+
+    var retval = setState(msgObj);
 
     //sending msg
-    server.send(message,remote.port,HOST,function(error){});
-
-
+    server.send("OK",remote.port,HOST,function(error){});
 });
 
-var myInt = setInterval(function () {
-    if(color == null){
-	    busylight.off();
-    } else {
-	    busylight.light(color.toString());
-    }
-}, 2000); 
 
+function setState(msgObj){
+	if(myTimer != null){
+		clearInterval(myTimer);
+	}
+			
 
+	var cmd = msgObj.cmd;
+	var retval = "Error";
 
+	myTimer = setInterval(function () {
+		switch(cmd){
+			case 'off':
+				busylight.off();
+			break;
+			case 'solid':
+				busylight.light(msgObj.color);
+			break;
+
+			case '':
+
+			break;
+		}
+	}
+}
 server.bind(PORT, HOST);
 
 
